529425b0831bba645956de8cc3922873f650471b
viansyah06 <viansyah321@gmail.com>
2025-04-26 22:19:44 +0700

    Mengaktifkan konfigurasi AppServiceProvider untuk Heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,31 +17,21 @@ class AppServiceProvider extends ServiceProvider
     {
         // Heroku DATABASE_URL Parser for PostgreSQL
         if (env('DATABASE_URL')) {
-            try {
-                $url = parse_url(env('DATABASE_URL'));
-                
-                Config::set('database.default', 'pgsql');
-                Config::set('database.connections.pgsql.host', $url['host'] ?? null);
-                Config::set('database.connections.pgsql.port', $url['port'] ?? null);
-                Config::set('database.connections.pgsql.database', substr($url['path'] ?? '', 1));
-                Config::set('database.connections.pgsql.username', $url['user'] ?? null);
-                Config::set('database.connections.pgsql.password', $url['pass'] ?? null);
-                Config::set('database.connections.pgsql.sslmode', 'require');
-                
-                // Backward compatibility with putenv
-                putenv('DB_HOST='.$url['host']);
-                putenv('DB_PORT='.$url['port']);
-                putenv('DB_USERNAME='.$url['user']);
-                putenv('DB_PASSWORD='.$url['pass']);
-                putenv('DB_DATABASE='.substr($url['path'], 1));
-                
-                // Debug info
-                if (env('APP_DEBUG')) {
-                    Log::debug('Database configuration set via DATABASE_URL');
-                }
-            } catch (\Exception $e) {
-                Log::error('Error parsing DATABASE_URL: ' . $e->getMessage());
-            }
+            $url = parse_url(env('DATABASE_URL'));
+            
+            Config::set('database.default', 'pgsql');
+            Config::set('database.connections.pgsql.host', $url['host']);
+            Config::set('database.connections.pgsql.port', $url['port']);
+            Config::set('database.connections.pgsql.database', substr($url['path'], 1));
+            Config::set('database.connections.pgsql.username', $url['user']);
+            Config::set('database.connections.pgsql.password', $url['pass']);
+            
+            // Backward compatibility with putenv
+            putenv('DB_HOST='.$url['host']);
+            putenv('DB_PORT='.$url['port']);
+            putenv('DB_USERNAME='.$url['user']);
+            putenv('DB_PASSWORD='.$url['pass']);
+            putenv('DB_DATABASE='.substr($url['path'], 1));
         }
     }
 
@@ -55,21 +44,7 @@ class AppServiceProvider extends ServiceProvider
             URL::forceScheme('https');
         }
         
-        // Enable CORS
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-        
         // Menghindari error "specified key was too long" di MariaDB/MySQL
         Schema::defaultStringLength(191);
-        
-        // Error handling untuk debugging
-        if (env('APP_DEBUG')) {
-            DB::listen(function($query) {
-                Log::debug($query->sql, [
-                    'bindings' => $query->bindings,
-                    'time' => $query->time
-                ]);
-            });
-        }
     }
 }
